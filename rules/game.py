eb89ea9300d918ed9cb49d753f9451cd45b25b88
Peter G <peter.c.gordon@gmail.com>
2025-08-29 14:06:50 -0600

    :)

--- rules/game.py
+++ rules/game.py
@@ -5,6 +5,41 @@ This is going to implement the PyQt6 stuff and add a new set of buttons and labe
 
 
 """
+class player:
+    def __init__(self,label, points = 0):
+        self.points = points
+        self.label = label
+        self.scores = {0:{}}
+        self.cur_round = 0
+
+    def update_points(self,points):
+        self.points = points
+        self.label.text = str(points)
+
+    def set_score_from_player(self, id :int, score : int):
+        def _helper():
+            self.scores[self.cur_round][id] = score
+            print(self.scores)
+        return  _helper
+
+    def set_next_round(self):
+        self.cur_round += 1
+        self.scores[self.cur_round] = {}
+    
+    def get_average_from_round(self, round: int):
+        if round in self.scores:
+            new_scores = list(self.scores[round].values())
+            total = 0
+            n = 0 
+            for score in new_scores:
+                n+=1
+                total += score
+            average = total / n
+            return average
+        else:
+            print("There is no such round logged")
+            return 0
+
 
 class game:
 
--- rules/pyqt_classes.py
+++ rules/pyqt_classes.py
@@ -8,20 +8,15 @@ from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QIcon
 
 import os
-
-class pointLabel(QLabel):
-
-    def __init__(self, text = ""):
-        super().__init__(text)
-    
-    def set_points(self, points:int):
-        self.text = str(points)
+import game
 
 
 class Window(QWidget):
-    def __init__(self, x = 300, y = 300, name = "My Window"):
+    def __init__(self, x = 300, y = 300, name = "My Window", player_num = 3):
         super().__init__()
-
+        self.cur_row = 0
+        self.players = {}
+        self.num_player = player_num
         abspath = "C:/Users/Mango/Desktop/Python_server_client/Python-server/"
         self.setWindowIcon(QIcon(abspath+ "Saturn.png"))
         self.setWindowTitle(name)
@@ -35,36 +30,75 @@ class Window(QWidget):
             pass
         self.setLayout(self.layout)
 
-        
+        for player in range(self.num_player):
+            self.add_player()
 
-        self.label1 = QLabel("Username: ")
-        self.layout.addWidget(self.label1, 0,0)
+        # self.label1 = QLabel("Username: ")
+        # self.layout.addWidget(self.label1, 0,0)
 
-        self.label1 = QLabel("Password: ")
-        self.layout.addWidget(self.label1, 1,0)
+        # self.label1 = QLabel("Password: ")
+        # self.layout.addWidget(self.label1, 1,0)
 
-        self.input1 = QLineEdit()
-        self.layout.addWidget(self.input1 ,0 , 1)
+        # self.input1 = QLineEdit()
+        # self.layout.addWidget(self.input1 ,0 , 1)
 
-        self.input2 = QLineEdit()
-        self.layout.addWidget(self.input2,1,1)
+        # self.input2 = QLineEdit()
+        # self.layout.addWidget(self.input2,1,1)
 
         self.button = QPushButton("Submit")
         self.button.setFixedWidth(50)
         self.button.clicked.connect(self.display)
-        self.layout.addWidget(self.button,2,1, Qt.AlignmentFlag.AlignRight)
+        self.layout.addWidget(self.button,self.cur_row,1, Qt.AlignmentFlag.AlignRight)
 
     def display(self):
-        print(self.input1.text())
-        print(self.input2.text())
-        sys.exit()
+        print("moving on")
+        for player in self.players:
+            self.players[player].set_next_round()
     
     def add_player(self):
-        pass
+        
+        label1 = QLabel("Player: " + str(self.cur_row))
+        self.layout.addWidget(label1, self.cur_row, 0)
+
+        new_label = QLabel("Points: " + str(0))
+        self.layout.addWidget(new_label, self.cur_row, 6)
+        
+        new_player = game.player(new_label)
+        self.players[self.cur_row] = new_player
+
+
+        button = QPushButton("Very Negative")
+        button.setFixedWidth(100)
+        button.clicked.connect(new_player.set_score_from_player(self.cur_row, -2))
+        self.layout.addWidget(button,self.cur_row,1)
+
+        button = QPushButton("Negative")
+        button.setFixedWidth(100)
+        button.clicked.connect(new_player.set_score_from_player(self.cur_row,-1))
+        self.layout.addWidget(button,self.cur_row,2)
+
+        button = QPushButton("Neutral")
+        button.setFixedWidth(100)
+        button.clicked.connect(new_player.set_score_from_player(self.cur_row,0))
+        self.layout.addWidget(button,self.cur_row,3)
+
+        button = QPushButton("Positive")
+        button.setFixedWidth(100)
+        button.clicked.connect(new_player.set_score_from_player(self.cur_row,1))
+        self.layout.addWidget(button,self.cur_row,4)
+
+        button = QPushButton("Very Positive")
+        button.setFixedWidth(100)
+        button.clicked.connect(new_player.set_score_from_player(self.cur_row,2))
+        self.layout.addWidget(button,self.cur_row,5)
+
+
+        self.cur_row += 1
+        # x5
         """
         add 5 buttons, and 2 labels
         name, buttons, points/reputation
-        
+
         """
 
 def main():
@@ -80,7 +114,7 @@ def set_style(app : QApplication, sheet : str):
         if not f:
             return False
         lines = f.read()
-        print(lines) 
+        # print(lines) 
         
         app.setStyleSheet(lines)
 
